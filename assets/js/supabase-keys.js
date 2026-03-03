44e551b5c0e5b858e16fa2370081062057632be6
vduy1210 <lenguyenvuduy123456@gmail.com>
2026-03-03 17:55:42 +0700

    Add supabase-keys.js with new music-db credentials

---
+++ assets/js/supabase-keys.js
@@ -0,0 +1,3 @@
+// Supabase credentials for local development
+window.SUPABASE_URL = 'https://vsyweyjsgypgaayuflrc.supabase.co'; // Thay bằng URL của bạn
+window.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWt4dnJxbXFtaGpsam9ocWVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1MjkwMjYsImV4cCI6MjA4ODEwNTAyNn0.KMW9_tB6boDuPf2pgwoIevtSjRf2g5HUDyaBr9Tlks8'; // Thay bằng key của bạn
